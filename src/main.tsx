966d32778c3f6558ed3c1133cb3b820531b08b2f
pawix135 <leagueoflegendsoppl@gmail.com>
2024-03-22 20:45:34 +0100

    hash router

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import KodPage from "./pages/kod/index.tsx";
 import TurnejPage from "./pages/turniej/index.tsx";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <App />,
